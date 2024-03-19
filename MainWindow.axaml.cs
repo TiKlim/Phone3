925caad7174d1c8e6572c24158d2ce83f5e8a27b
TiKlim <karelian_company@mail.ru>
2024-03-19 12:30:45 +0300

    19.03.2024/12:30

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -9,17 +9,14 @@ namespace Phone3
         public MainWindow()
         {
             InitializeComponent();
-
             Butoon.Click += Butoon_Click;
-
-       
         }
 
         private void Butoon_Click(object? sender, RoutedEventArgs e)
         {
             if (a9.IsChecked == true && a15.IsChecked == true && a16.IsChecked == true && a17.IsChecked == true && a18.IsChecked == true && a19.IsChecked == true && a20.IsChecked == true && a21.IsChecked == true)
             {
-                message.Text = "1";
+                message.Text = "("+"1";
             }
             if (a30.IsChecked == true && a31.IsChecked == true && a34.IsChecked == true && a35.IsChecked == true && a42.IsChecked == true && a49.IsChecked == true && a56.IsChecked == true && a36.IsChecked == true && a43.IsChecked == true && a51.IsChecked == true && a52.IsChecked == true && a46.IsChecked == true && a40.IsChecked == true)
             {
@@ -27,7 +24,7 @@ namespace Phone3
             }
             if (a58.IsChecked == true && a64.IsChecked == true && a71.IsChecked == true && a79.IsChecked == true && a80.IsChecked == true && a74.IsChecked == true && a67.IsChecked == true && a82.IsChecked == true && a83.IsChecked == true && a70.IsChecked == true && a77.IsChecked == true && a62.IsChecked == true)
             {
-                message.Text += "3";
+                message.Text += "3"+")";
             }
             if (a85.IsChecked == true && a86.IsChecked == true && a87.IsChecked == true && a88.IsChecked == true && a95.IsChecked == true && a102.IsChecked == true && a109.IsChecked == true && a105.IsChecked == true && a104.IsChecked == true && a103.IsChecked == true && a102.IsChecked == true && a101.IsChecked == true && a100.IsChecked == true)
             {
@@ -39,9 +36,9 @@ namespace Phone3
             }
             if (a142.IsChecked == true && a143.IsChecked == true && a144.IsChecked == true && a145.IsChecked == true && a146.IsChecked == true && a148.IsChecked == true && a155.IsChecked == true && a163.IsChecked == true && a154.IsChecked == true && a161.IsChecked == true && a167.IsChecked == true && a166.IsChecked == true && a158.IsChecked == true && a151.IsChecked == true)
             {
-                message.Text += "6";
+                message.Text += "6"+"-";
             }
-            if (a170.IsChecked == true && a169.IsChecked == true && a176.IsChecked == true && a183.IsChecked == true && a190.IsChecked == true && a191.IsChecked == true && a192.IsChecked == true && a187.IsChecked == true && a188.IsChecked == true && a181.IsChecked == true && a182.IsChecked == true)
+            if (a169.IsChecked == true && a170.IsChecked == true && a176.IsChecked == true && a183.IsChecked == true && a190.IsChecked == true && a191.IsChecked == true && a192.IsChecked == true && a182.IsChecked == true && a181.IsChecked == true && a187.IsChecked == true && a186.IsChecked == true)
             {
                 message.Text += "7";
             }
